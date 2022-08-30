a7966048f894f7ef0d4a418182e811f803423dae
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-08-30 15:37:42 +0200

    added-snack1

---
+++ snack1.php
@@ -0,0 +1,12 @@
+<?php
+$matches = [
+    'match1' => 'Olimpia Milano - Cantù | 55-60',
+    'match2' => 'Virtus Bologna - Fortitudo Bologna | 72-80',
+    'match3' => 'Pallacanestro Varese - Pallacanestro Reggiana | 70-50',
+    ];
+echo $matches['match1'];
+echo $matches['match2'];
+echo $matches['match3'];
+?>
+
+
