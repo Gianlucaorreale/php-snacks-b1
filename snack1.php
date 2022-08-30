5b1fee35c0a0bead381dba29e15ff8167b06d57d
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-08-30 16:52:49 +0200

    fixed-snack-1

--- snack1.php
+++ snack1.php
@@ -1,12 +1,31 @@
 <?php
 $matches = [
-    'match1' => 'Olimpia Milano - Cantù | 55-60',
-    'match2' => 'Virtus Bologna - Fortitudo Bologna | 72-80',
-    'match3' => 'Pallacanestro Varese - Pallacanestro Reggiana | 70-50',
+     'Olimpia Milano - Cantù | 55-60',
+     'Virtus Bologna - Fortitudo Bologna | 72-80',
+     'Pallacanestro Varese - Pallacanestro Reggiana | 70-50',
     ];
-echo $matches['match1'];
-echo $matches['match2'];
-echo $matches['match3'];
+//echo $matches['match1'];
+//echo $matches['match2'];
+//echo $matches['match3'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+       <?php for($i = 0; $i < count($matches); $i++) : ?>
+        <li>
+           <p> <?= $matches[$i] ?> </p>
+        </li>
+       <?php endfor; ?>
+    </ul>
+</body>
+</html>
 
 
